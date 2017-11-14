bbca87f7e34635abdf09d91bb4b451a7ba9d73ee
danielbkatz <katzdanb@gmail.com>
2017-11-14 13:28:18 -0800

    let's you create text files

--- bfilescript.R
+++ bfilescript.R
@@ -1,9 +1,11 @@
-bfile <- function(name, x, filextension){
-  name <- deparse(substitute(name))
+batchfile <- function(name1, x, filextension, filesave){
+  name1 <- deparse(substitute(name1))
   filextension <- deparse(substitute(filextension))
+  filesave <- deparse(substitute(filesave))
   ii <- 1:x
-  teststring <- paste("call mplus.exe ", name, ii, filextension, sep = "")
-  filetype <- paste(name, filextension, sep = "")
+  teststring <- paste("call mplus.exe ", name1, ii, filextension, sep = "")
+  filetype <- paste(name1, filesave, sep = "")
   cat(teststring, file=filetype, sep="\n")
 }
+
 
